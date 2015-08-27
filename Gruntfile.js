45731f01cedd6d839a8a2ae260d8ef85975a02eb
Luca Tumedei <luca@theaveragedev.com>
2015-08-27 14:03:50 +0200

    composer update

--- Gruntfile.js
+++ Gruntfile.js
@@ -1,16 +1,22 @@
 module.exports = function ( grunt ) {
 
-	var dependencies = ["vendor/lucatume/klein52"],
-		delete_patterns = [".git/**", "tests/**", ".gitignore", "**.md", "Gruntfile.js", "example-functions.php", "composer.{json,lock}", "{.travis,.scrutinizer,codeception*,}.yml", "coverage.clover", "phpunit.xml.dist"],
-		clean_dist_patterns = [],
-		git_add_patterns = [];
+	var composer = grunt.file.readJSON( 'composer.json' ),
+		dependencies = [];
 
-	for ( i = 0; i < dependencies.length; i++ ) {
-		git_add_patterns.push( dependencies[i] + '**' );
-		for ( k = 0; k < delete_patterns.length; k++ ) {
-			clean_dist_patterns.push( dependencies[i] + '/' + delete_patterns[k] );
-		}
+	for ( var dep in composer.require ) {
+		dependencies.push( 'vendor/' + dep );
 	}
+
+	var delete_patterns = [".git/**", "tests/**", ".gitignore", "**.md", "Gruntfile.js", "example-functions.php", "composer.{json,lock}", "{.travis,.scrutinizer,codeception*,}.yml", "coverage.clover", "phpunit.xml.dist"],
+		clean_dist_patterns = ['vendor/composer/installed.json'],
+		git_add_patterns = ['vendor/autoload*.php', 'vendor/composer/{autoload_*,ClassLoader*}.php'];
+
+		for ( i = 0; i < dependencies.length; i++ ) {
+			git_add_patterns.push( dependencies[i] + '**' );
+			for ( k = 0; k < delete_patterns.length; k++ ) {
+				clean_dist_patterns.push( dependencies[i] + '/' + delete_patterns[k] );
+			}
+		}
 
 	grunt.initConfig( {
 		pkg: grunt.file.readJSON( 'package.json' ),
