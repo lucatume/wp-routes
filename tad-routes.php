00c6c10c7c99e384f5364d1cffe07f25c3ba1c6d
Luca Tumedei <luca@theaveragedev.com>
2015-08-27 11:25:48 +0200

    updated code to work as base plugin

--- tad-routes.php
+++ tad-routes.php
@@ -12,33 +12,26 @@
 require 'vendor/autoload_52.php';
 
 /**
- * Parse request
+ * Allows theme and plugin developers to handle the routing before WordPress does.
+ *
+ * @param       $continue
+ * @param    WP $wp
+ * @param       $extra_query_vars
+ *
+ * @return mixed
  */
-add_filter( 'do_parse_request', 'tad_routes_do_parse_request', 1, 3 );
 function tad_routes_do_parse_request( $continue, WP $wp, $extra_query_vars ) {
-	respond( '*', function () {
-		include locate_template( 'header.php', false );
-	} );
+	/**
+	 * Allow plugin and theme developers to register their own routes.
+	 */
+	do_action( 'tad/routes/register_routes' );
 
-	respond( '/hello/[a:name]', function ( $request ) {
-		echo "<div id=\"primary\" class=\"content-area\">
-			<main id=\"main\" class=\"site-main\" role=\"main\">
-				<article class=\"hentry\">
-					<header class=\"entry-header\">
-						<h2 class=\"entry-title\">Hi {$request->name}</h2></header>
-					<div class=\"entry-content\">
-						<p>Lorem ipsum dolor sit amet</p>
-					</div>
-				</article>
-			</main>
-		</div>";
-	} );
-
-	respond( '*', function () {
-		include locate_template( 'footer.php', false );
-	} );
-
+	// if no echo was produced or the only echo produced is from "*" routes
+	// then continue and let WordPress handle the request; otherwise `die` the
+	// route output.
 	dispatch_or_continue();
 
 	return $continue;
 }
+
+add_filter( 'do_parse_request', 'tad_routes_do_parse_request', 1, 3 );
