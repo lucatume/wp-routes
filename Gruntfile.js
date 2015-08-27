5f4428ecfc3979df5b84d32fd0fbececbbf1a675
Luca Tumedei <luca@theaveragedev.com>
2015-08-27 11:36:14 +0200

    composer update

--- Gruntfile.js
+++ Gruntfile.js
@@ -1,21 +1,22 @@
 module.exports = function ( grunt ) {
 
-	var dependencies = ["vendor/lucatume/klein52/"],
-		delete_patterns = [".git/**", "tests/**", ".gitignore","**.md", "Gruntfile.js", "example-functions.php", "composer.{json,lock}", "{.travis,.scrutinizer,codeception*,}.yml", "coverage.clover", "phpunit.xml.dist"],
+	var dependencies = ["vendor/lucatume/klein52"],
+		delete_patterns = [".git/**", "tests/**", ".gitignore", "**.md", "Gruntfile.js", "example-functions.php", "composer.{json,lock}", "{.travis,.scrutinizer,codeception*,}.yml", "coverage.clover", "phpunit.xml.dist"],
 		clean_dist_patterns = [],
 		git_add_patterns = [];
 
 	for ( i = 0; i < dependencies.length; i++ ) {
 		git_add_patterns.push( dependencies[i] + '**' );
 		for ( k = 0; k < delete_patterns.length; k++ ) {
-			clean_dist_patterns.push( dependencies[i] + delete_patterns[k] );
+			clean_dist_patterns.push( dependencies[i] + '/' + delete_patterns[k] );
 		}
 	}
 
 	grunt.initConfig( {
 		pkg: grunt.file.readJSON( 'package.json' ),
 		clean: {
-			dist: clean_dist_patterns
+			dist: clean_dist_patterns,
+			'pre-update': dependencies
 		},
 		gitadd: {
 			dist: {
@@ -33,5 +34,6 @@ module.exports = function ( grunt ) {
 	grunt.loadNpmTasks( 'grunt-contrib-clean' );
 	grunt.loadNpmTasks( 'grunt-git' );
 
+	grunt.registerTask( 'pre-composer-update', ['clean:pre-update'] );
 	grunt.registerTask( 'after-composer-update', ['clean:dist', 'gitadd:dist'] );
 };
