93618e9a4cfad7d8132dd164c34b1bd3673e4791
Luca Tumedei <luca@theaveragedev.com>
2015-08-28 08:51:22 +0200

    updated doc comments

--- tad-routes.php
+++ tad-routes.php
@@ -12,23 +12,32 @@
 require 'vendor/autoload_52.php';
 
 /**
- * Allows theme and plugin developers to handle the routing before WordPress does.
+ * Filters the request parsing process before WordPress does.
  *
- * @param       $continue
- * @param    WP $wp
- * @param       $extra_query_vars
+ * @param bool         $continue
+ * @param WP           $wp
+ * @param string|array $extra_query_vars
  *
- * @return mixed
+ * @return bool|void Either a bool for the `$continue` value or void if the parse request is stopped.
  */
 function tad_routes_do_parse_request( $continue, WP $wp, $extra_query_vars ) {
 	/**
-	 * Allow plugin and theme developers to register their own routes.
+	 * Allows plugin and theme developers to register custom routes to be handled before WordPress
+	 * parses the request.
+	 *
+	 * @since 1.0.0
+	 *
+	 * @param bool         $bool             Whether or not to parse the request. Default true.
+	 * @param WP           $this             Current WordPress environment instance.
+	 * @param array|string $extra_query_vars Extra passed query variables.
 	 */
-	do_action( 'tad/routes/register_routes' );
+	do_action( 'tad/routes/register_routes', $continue, $wp, $extra_query_vars );
 
-	// if no echo was produced or the only echo produced is from "*" routes
-	// then continue and let WordPress handle the request; otherwise `die` the
-	// route output.
+	/**
+	 * if no echo was produced or the only echo produced is from "*" routes
+	 * then continue and let WordPress handle the request; otherwise `die` the
+	 * route output.
+	 */
 	dispatch_or_continue();
 
 	return $continue;
