11bc02bb8146d449ed6e3907843a50ef2f1d6eef
Luca Tumedei <luca@theaveragedev.com>
2015-09-12 16:44:42 +0200

    added missing function existence check

--- wp-routes.php
+++ wp-routes.php
@@ -11,36 +11,38 @@
 
 require 'vendor/autoload_52.php';
 
-/**
- * Filters the request parsing process before WordPress does.
- *
- * @param bool         $continue
- * @param WP           $wp
- * @param string|array $extra_query_vars
- *
- * @return bool|void Either a bool for the `$continue` value or void if the parse request is stopped.
- */
-function wp_routes_do_parse_request( $continue, WP $wp, $extra_query_vars ) {
+if (!function_exists('wp_routes_do_parse_request')) {
 	/**
-	 * Allows plugin and theme developers to register custom routes to be handled before WordPress
-	 * parses the request.
+	 * Filters the request parsing process before WordPress does.
 	 *
-	 * @since 1.0.0
+	 * @param bool         $continue
+	 * @param WP           $wp
+	 * @param string|array $extra_query_vars
 	 *
-	 * @param bool         $bool             Whether or not to parse the request. Default true.
-	 * @param WP           $this             Current WordPress environment instance.
-	 * @param array|string $extra_query_vars Extra passed query variables.
+	 * @return bool|void Either a bool for the `$continue` value or void if the parse request is stopped.
 	 */
-	do_action( 'wp-routes/register_routes', $continue, $wp, $extra_query_vars );
+	function wp_routes_do_parse_request( $continue, WP $wp, $extra_query_vars ) {
+		/**
+		 * Allows plugin and theme developers to register custom routes to be handled before WordPress
+		 * parses the request.
+		 *
+		 * @since 1.0.0
+		 *
+		 * @param bool         $bool             Whether or not to parse the request. Default true.
+		 * @param WP           $this             Current WordPress environment instance.
+		 * @param array|string $extra_query_vars Extra passed query variables.
+		 */
+		do_action( 'wp-routes/register_routes', $continue, $wp, $extra_query_vars );
 
-	/**
-	 * if no echo was produced or the only echo produced is from "*" routes
-	 * then continue and let WordPress handle the request; otherwise `die` the
-	 * route output.
-	 */
-	dispatch_or_continue();
+		/**
+		 * if no echo was produced or the only echo produced is from "*" routes
+		 * then continue and let WordPress handle the request; otherwise `die` the
+		 * route output.
+		 */
+		dispatch_or_continue();
 
-	return $continue;
+		return $continue;
+	}
+
+	add_filter( 'do_parse_request', 'wp_routes_do_parse_request', 1, 3 );
 }
-
-add_filter( 'do_parse_request', 'wp_routes_do_parse_request', 1, 3 );
